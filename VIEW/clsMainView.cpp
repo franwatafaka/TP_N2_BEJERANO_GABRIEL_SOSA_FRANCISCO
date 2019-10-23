88117daf32a8a835bc591ef10e0b440f3233d023
Raptor <37874224+alienthreat@users.noreply.github.com>
2019-10-23 13:04:59 -0300

    UPDATE

--- VIEW/clsMainView.cpp
+++ VIEW/clsMainView.cpp
@@ -1,6 +1,7 @@
 #include "clsMainView.h"
 #include "clsMateriasView.h"
 #include "clsAlumnosView.h"
+#include "../BL/clsValidacionesBL.h"
 
 using namespace std;
 
@@ -8,35 +9,35 @@ using namespace std;
 
 void clsMainView::Menu()
 {
- char op;
- bool     salir=false;
+ char *op;
+ clsValidacionesBL val;
+ bool salir=false;
     cout<<"----------MENU---------"<<endl;
-    cout<<"M - Materias"<<endl;
-    cout<<"A - Alumnos"<<endl;
-    cout<<"S - Salir"<<endl;
+    cout<<" 1- Materias"<<endl;
+    cout<<" 2 - Alumnos"<<endl;
+    cout<<" 9 - Salir"<<endl;
     cout<<"-----------------------"<<endl;
     cin>>op;
+    cout<<op<<endl;
+    system("pause");
     cin.ignore();
     system("cls");
-    switch(op)
+    switch(val.validarUnNumero(op))
     {
-        case 'm':
-        case'M' :
+        case 1:
         {
           clsMateriasView matView;
           matView.MenuMaterias();
         }
         break;
-        case 'a':
-        case'A' :
+        case 2:
         {
           clsAlumnosView aluView;
           aluView.MenuAlumnos();
         }
-        case's':
-        case'S':
+        case 9:
         {
-
+        salir=true;
         }
         break;
         default:
